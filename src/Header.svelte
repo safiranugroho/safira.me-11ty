742cb77a532c9d8d816b556cd6d1fa3a548c0dca
Safira Nugroho <snugroho@wesdigital.com.au>
2023-01-16 18:29:15 +1100

    add keyboard solution

---
+++ src/Header.svelte
@@ -0,0 +1,55 @@
+<script>
+	/**
+	 * @type {string}
+	 */
+	export let heading;
+	/**
+	 * @type {string}
+	 */
+	export let subheading;
+	/**
+	 * @type {string}
+	 */
+	export let completedDate;
+</script>
+
+<div class="header">
+	<h1>{heading}</h1>
+	<h2>{subheading}</h2>
+	<p>{completedDate}</p>
+</div>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Noto+Serif+Toto:wght@400;700&display=swap');
+
+	:global(body) {
+		font-family: 'Noto Serif Toto', serif;
+	}
+
+	:global(h1) {
+		font-weight: 700;
+		font-size: 20px;
+		line-height: 17px;
+		font-style: italic;
+		color: #525252;
+	}
+
+	:global(h2) {
+		font-weight: 400;
+		font-size: 14px;
+		line-height: 12px;
+		color: #525252;
+	}
+
+	:global(p) {
+		font-weight: 400;
+		font-size: 12px;
+		line-height: 10px;
+		color: #525252;
+	}
+
+	.header {
+		margin-top: 60px;
+		text-align: center;
+	}
+</style>
---
+++ src/routes/keyboard/+page.svelte
@@ -0,0 +1,190 @@
+<script>
+	import { onMount } from 'svelte';
+	import Header from '../../Header.svelte';
+
+	const keys = ['`1234567890-=', 'qwertyuiop[]\\', "asdfghjkl;'", 'zxcvbnm,./'];
+	const modifiers = ['backspace', 'tab', 'enter', 'capslock', 'enter', 'shift'];
+	const allKeys = keys.flatMap((row) => row.toLowerCase().split('')).concat(modifiers);
+
+	let prev = Math.random() * allKeys.length;
+	const getRandomIndex = () => {
+		let randomIndex = prev;
+
+		while (randomIndex == prev) {
+			randomIndex = Math.floor(Math.random() * allKeys.length);
+		}
+
+		prev = randomIndex;
+		return randomIndex;
+	};
+
+	/** @type {Record<string, string>} */
+	const map = {};
+	const getSelector = (/** @type {string} */ id) => {
+		if (map[id]) return map[id];
+
+		if (id.length > 1 || ('a' <= id && 'z' >= id)) {
+			map[id] = `#${id}`;
+		} else if ('0' <= id && '9' >= id) {
+			map[id] = `#\\3${id}`;
+		} else {
+			map[id] = `#\\${id}`;
+		}
+
+		return map[id];
+	};
+
+	const addJiggle = () => {
+		const randomIndex = getRandomIndex();
+		const selector = getSelector(allKeys[randomIndex]);
+		const key = document.querySelector(selector);
+
+		key?.classList.add('jiggle');
+	};
+
+	const removeJiggle = (/** @type {string} */ id) => {
+		const selector = getSelector(id);
+		const prevKey = document.querySelector(selector);
+		prevKey?.classList.remove('jiggle');
+	};
+
+	onMount(() => {
+		addJiggle();
+
+		document.addEventListener('keydown', (e) => {
+			if (allKeys[prev] == e.key.toLowerCase()) {
+				removeJiggle(e.key.toLowerCase());
+				addJiggle();
+			}
+		});
+	});
+</script>
+
+<Header
+	heading="My keyboard jiggles"
+	subheading="Press the key on your keyboard to stop the jiggle."
+	completedDate="16 January 2023"
+/>
+
+<div class="content">
+	<div class="container">
+		<div class="row">
+			{#each keys[0] as char}
+				<div class="key" id={char}>{char}</div>
+			{/each}
+			<div class="key modifier" id="backspace">del</div>
+		</div>
+		<div class="row">
+			<div class="key modifier" id="tab">tab</div>
+			{#each keys[1] as char}
+				<div class="key" id={char}>{char}</div>
+			{/each}
+		</div>
+		<div class="row">
+			<div class="key modifier" id="capslock">caps</div>
+			{#each keys[2] as char}
+				<div class="key" id={char}>{char}</div>
+			{/each}
+			<div class="key modifier" id="enter">enter</div>
+		</div>
+		<div class="row">
+			<div class="key modifier" id="shift">shift</div>
+			{#each keys[3] as char}
+				<div class="key" id={char}>{char}</div>
+			{/each}
+			<div class="key modifier" id="shift">shift</div>
+		</div>
+	</div>
+</div>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap');
+
+	:global(body) {
+		display: grid;
+		grid-template-rows: 1fr 2fr;
+	}
+
+	.content {
+		padding: 16px;
+	}
+
+	.container {
+		font-family: 'Inter', sans-serif;
+		font-size: 24px;
+
+		background: #f6f6f6;
+		border-radius: 40px;
+
+		padding: 43px 50px;
+
+		display: flex;
+		flex-direction: column;
+		gap: 12px;
+
+		text-transform: uppercase;
+
+		max-width: 1200px;
+		margin: auto;
+	}
+
+	.row {
+		display: grid;
+		gap: 12px;
+	}
+
+	.row:nth-of-type(1) {
+		grid-template-columns: repeat(13, 1fr) 1.67fr;
+	}
+
+	.row:nth-of-type(2) {
+		grid-template-columns: 1.67fr repeat(13, 1fr);
+	}
+
+	.row:nth-of-type(3) {
+		grid-template-columns: 2fr repeat(11, 1fr) 1.8fr;
+	}
+
+	.row:nth-of-type(4) {
+		grid-template-columns: 2.5fr repeat(10, 1fr) 2.5fr;
+	}
+
+	.key {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+
+		text-align: center;
+
+		background: #ffffff;
+		border-radius: 16px;
+		height: 65px;
+		color: #60c1b6;
+	}
+
+	.modifier {
+		color: #868888;
+	}
+
+	:global(.jiggle) {
+		animation: jiggle 0.25s infinite;
+	}
+
+	@keyframes jiggle {
+		0% {
+			transform: translate(0, 0) rotate(0deg);
+		}
+		25% {
+			transform: translate(5px, 5px) rotate(5deg);
+		}
+		50% {
+			transform: translate(0, 0) rotate(0eg);
+		}
+		75% {
+			transform: translate(-5px, 5px) rotate(-5deg);
+		}
+		100% {
+			transform: translate(0, 0) rotate(0deg);
+		}
+	}
+</style>
