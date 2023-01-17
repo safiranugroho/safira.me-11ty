de2b1defdce382665bd0ba31464c57abcddf0499
Safira Nugroho <snugroho@wesdigital.com.au>
2023-01-17 21:29:35 +1100

    edit site description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,13 +28,6 @@
   <div slot="content" class="content">
     <h1 class="title">Doodles by Safira. 🖍️</h1>
     <p>A collection of HTML/CSS/JS doodles, inspired by various prompts.</p>
-    <p>In addition to fulfilling the prompt, I aim to produce solutions that are also:</p>
-    <ul>
-      <li>Accessible (readable by screen reader and navigatable by keyboard at minimum)</li>
-      <li>
-        Responsive (mobile, tablet, and desktop views with minimum supported dimension of 320px)
-      </li>
-    </ul>
     <div class="doodle-list">
       {#each doodles as { path, name, cover }}
         <div class="doodle">
