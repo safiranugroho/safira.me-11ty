756824b0dac32845aad15c7dd8bc2a0b20198f40
Safira Nugroho <snugroho@wesdigital.com.au>
2023-01-22 14:27:25 +1100

    add inputs for filter

--- src/routes/filter/+page.svelte
+++ src/routes/filter/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-  // @ts-nocheck
+  import Page from '$lib/Page.svelte';
 
   /**
    * TODO:
@@ -8,50 +8,127 @@
    * 3. Short description of what each filter is
    */
 
+  /** @type {Record<string, any>}*/
   const filters = {
-    blur: 'blur(5px)',
-    brightness: 'brightness(0.4)',
-    contrast: 'contrast(200%)',
-    dropShadow: 'drop-shadow(16px 16px 20px blue)',
-    grayscale: 'grayscale(50%)',
-    hueRotate: 'hue-rotate(90deg)',
-    invert: 'invert(75%)',
-    opacity: 'opacity(25%)',
-    saturate: 'saturate(30%)',
-    sepia: 'sepia(60%)'
+    blur: {
+      unit: 'px',
+      default: 0,
+      min: 0,
+      max: 50
+    },
+    brightness: {
+      unit: '%',
+      default: 100,
+      min: 0,
+      max: 200
+    },
+    contrast: {
+      unit: '%',
+      default: 100,
+      min: 0,
+      max: 200
+    },
+    grayscale: {
+      unit: '%',
+      default: 0,
+      min: 0,
+      max: 100
+    },
+    'hue-rotate': {
+      unit: 'deg',
+      default: 0,
+      min: 0,
+      max: 360
+    },
+    invert: {
+      unit: '%',
+      default: 0,
+      min: 0,
+      max: 100
+    },
+    opacity: {
+      unit: '%',
+      default: 100,
+      min: 0,
+      max: 100
+    },
+    saturate: {
+      unit: '%',
+      default: 0,
+      min: 0,
+      max: 100
+    },
+    sepia: {
+      unit: '%',
+      default: 0,
+      min: 0,
+      max: 100
+    }
   };
 
-  let selectedFilter = '';
+  /** @type Array<string> */
+  let all = [];
+
+  /** @type {Record<string, any>}*/
+  let sizes = {};
+
+  const getSizes = () =>
+    (sizes = all.length
+      ? all.reduce(
+          (a, s) => ({
+            ...a,
+            [s]: sizes[s] ?? { ...filters[s], value: filters[s].default }
+          }),
+          {}
+        )
+      : {});
+
+  $: css = Object.entries(sizes).length
+    ? Object.entries(sizes).reduce((a, [name, s]) => a + ' ' + `${name}(${s.value}${s.unit})`, '')
+    : 'none';
 </script>
 
-<div class="container">
-  <div class="circles" style="--filter: {filters[selectedFilter]}">
-    <div class="circle red" />
-    <div class="circle yellow" />
-    <div class="circle blue" />
+<Page>
+  <div class="container" slot="content">
+    <p>Dimensions: 50px</p>
+    <div class="circles" style="--filter: {css}">
+      <div class="circle red" />
+      <div class="circle yellow" />
+      <div class="circle blue" />
+    </div>
+    <div class="filter-input">
+      {#each Object.keys(filters) as filter}
+        <label
+          ><input
+            type="checkbox"
+            bind:group={all}
+            name="filter"
+            value={filter}
+            on:change={getSizes}
+          />{filter}</label
+        >
+      {/each}
+      <p>filter: {css}</p>
+      {#each Object.entries(sizes) as [s, a]}
+        <div class="range">
+          <p>{s}: {a.value}{a.unit}</p>
+          <span class="min">{a.min}{a.unit}</span><input
+            type="range"
+            bind:value={a.value}
+            min={a.min}
+            max={a.max}
+          /><span class="max">{a.max}{a.unit}</span>
+        </div>
+      {/each}
+    </div>
   </div>
-  <select name="filter" class="filter" id="filter" bind:value={selectedFilter}>
-    <option value="">Select a filter</option>
-    {#each Object.keys(filters) as filter}
-      <option value={filter}>{filter}</option>
-    {/each}
-  </select>
-</div>
+</Page>
 
 <style>
-  .container {
-    height: 100vh;
-    width: 50%;
-    margin: auto;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-  }
-
   .circles {
     display: flex;
     flex-direction: row;
+    justify-content: center;
     gap: 24px;
 
     margin: auto;
@@ -76,7 +153,15 @@
     background: blue;
   }
 
-  .filter {
-    margin: 64px;
+  .filter-input {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .range {
+    display: flex;
+    gap: 12px;
+    align-items: center;
   }
 </style>
