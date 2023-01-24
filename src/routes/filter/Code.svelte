59a0a6ae22af6ae7720de6ab79f8762f7fb1ca6e
Safira Nugroho <snugroho@wesdigital.com.au>
2023-01-24 15:03:48 +1100

    add copy to clipboard

--- src/routes/filter/Code.svelte
+++ src/routes/filter/Code.svelte
@@ -1,16 +1,37 @@
 <script lang="ts">
   import Prism from 'prismjs';
+  import { createEventDispatcher } from 'svelte';
+
+  import copyIcon from '$lib/assets/copy.png';
 
   export let content: string;
   export let language: string;
+
+  const dispatch = createEventDispatcher();
+  const copy = async () => {
+    try {
+      await navigator.clipboard.writeText(content);
+    } catch (e) {
+      console.log(e);
+    }
+
+    dispatch('copy');
+  };
 </script>
 
 <div class="code">
-  {@html Prism.highlight(content, Prism.languages[language], language)}
+  <div class="code-content">
+    {@html Prism.highlight(content, Prism.languages[language], language)}
+  </div>
+  <button on:click={copy} class="code-copy-button"
+    ><img src={copyIcon} alt="Copy to clipboard" class="code-copy-icon" /></button
+  >
 </div>
 
 <style>
   .code {
+    position: relative;
+
     padding: 16px;
     width: 100%;
 
@@ -18,5 +39,22 @@
     border-radius: 8px;
 
     font-family: var(--code-font-family);
+
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .code-copy-button {
+    position: absolute;
+    top: -1px;
+    right: -1px;
+
+    padding: 4px;
+  }
+
+  .code-copy-icon {
+    width: 20px;
+    height: 20px;
+    opacity: 0.8;
   }
 </style>
