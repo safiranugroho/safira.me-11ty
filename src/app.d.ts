412f80b089257ef1e36325e94a4ab9fd654364d9
Safira Nugroho <safiranugroho@gmail.com>
2023-02-05 18:27:40 +1100

    add quagga poc

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,15 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
+declare module 'quagga';
+
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface Platform {}
-	}
+  namespace App {
+    // interface Error {}
+    // interface Locals {}
+    // interface PageData {}
+    // interface Platform {}
+  }
 }
 
 export {};
---
+++ src/routes/allergens/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Page from '$lib/Page.svelte';
+  import Scanner from './Scanner.svelte';
+
+  $: barcode = '';
+  const onDetected = (code: string) => {
+    barcode = code;
+  };
+</script>
+
+<Page>
+  <div slot="content">
+    <Scanner {onDetected} />
+    <p>{barcode}</p>
+  </div>
+</Page>
